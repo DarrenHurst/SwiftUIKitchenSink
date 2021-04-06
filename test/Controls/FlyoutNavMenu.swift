ea9cdcf7133abe075170827e495dd3a6e1bf6ab0
Darren Hurst <Hurst_Darren@hotmail.com>
2021-04-06 13:50:26 -0400

    sync

--- test/Controls/FlyoutNavMenu.swift
+++ test/Controls/FlyoutNavMenu.swift
@@ -24,32 +24,51 @@ struct FlyoutNavMenu: View {
     var body: some View {
      
          
-                let item:FlyoutNavMenuItem = FlyoutNavMenuItem(title: "Accounts", systemName: "person")
-                
-                let items:[FlyoutNavMenuItem] = [item, item, item]
+        let item:FlyoutNavMenuItem = FlyoutNavMenuItem(title: "Accounts", systemName: "briefcase")
+        let item2:FlyoutNavMenuItem = FlyoutNavMenuItem(title: "Pay Bills", systemName: "arrow.right.arrow.left.square.fill")
+        let item3:FlyoutNavMenuItem = FlyoutNavMenuItem(title: "Transfer Money", systemName: "arrow.right.square.fill")
+        
+                let items:[FlyoutNavMenuItem] = [item, item2, item3]
         
         VStack(alignment: .leading) {
-           
+            HStack {
+                Rectangle()
+                    .fill(Color.green)
+                    .background( LinearGradient(gradient: Gradient(colors: [.green, .black]), startPoint: .top, endPoint: .bottom))
+                    .frame(width: UIScreen.screenWidth/2+35, height: 200, alignment: .top).offset(x:-20,y:-40)
+                    
+            }
                 ForEach(items, id: \.self) { item in
                   
                     HStack {
                         Image(systemName:item.systemName)
-                            .foregroundColor(.gray)
-                            .imageScale(.large)
+                            .foregroundColor(.black)
+                            .imageScale(.medium)
                         Text(item.title)
-                            .foregroundColor(.gray)
+                            .foregroundColor(.black)
                             .font(.TDSmall)
-                    }.padding(.top, 30).frame(height:80)
-                    
-                }
-                Spacer()
-        } .padding()
+                            .frame(width:UIScreen.screenWidth/2, alignment:.leading)
+                    }.padding(.top, 10)
+                    .padding(.bottom, 10)
+                    .padding(.leading, 45)
+                    .frame(width:UIScreen.screenWidth/2 - 40)
+                    .scaledToFit()
+                } .background(Rectangle().fill(Color.white).frame(width: UIScreen.screenWidth/2+20, height: 70, alignment: .center).cornerRadius(3.0)).offset(x:25,y:-30)
+                Spacer().background(Color.white)
+        } .padding(.leading,20)
         .frame(maxWidth: .infinity, alignment: .leading)
-        .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+        .background(Color.TDLightGray)
         .edgesIgnoringSafeArea(.all)
         
         
     
     }
     
+}
+
+
+struct FlyoutNavPreview: PreviewProvider {
+    static var previews: some View {
+      FlyoutNavMenu()
+    }
 }
--- test/Extensions/Color.swift
+++ test/Extensions/Color.swift
@@ -18,4 +18,8 @@ extension Color {
         return Color(UIColor(red: 219/255, green: 175/255, blue: 15/255, alpha: 1.0))
     }
     
+    public static var DarkGrey: Color {
+       return Color(red: 32/255, green: 32/255, blue: 32/255)
+    }
+    
 }
